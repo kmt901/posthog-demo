cf7da143fff1be365ff58633eac5f63627db4dc5
posthogseb <sebastian@posthog.com>
2024-08-14 13:56:59 +0100

    pop_db.py now with correct images and titles

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ph:r45se@localhost/ph_demo'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://3000:3000@localhost/3000'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
     WTF_CSRF_ENABLED = True
