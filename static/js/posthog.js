22d1079ec3a754af1546cb3e5a1b9bcfc7dc4598
David Rhodes <david@espressocode.tech>
2025-01-14 16:32:01 +0000

    feat(identify): sets person_profiles to always setting

--- static/js/posthog.js
+++ static/js/posthog.js
@@ -1,7 +1,15 @@
-// Ensure the posthog-js library is correctly imported
+// posthog.js
+
 import posthog from 'posthog-js';
 
-posthog.init('phc_riCzwyMkRO7qRnhNLYfUIAehPc6V0U7Qudkv1a1XLu0', { api_host: 'https://eu.i.posthog.com' });
+posthog.init('phc_riCzwyMkRO7qRnhNLYfUIAehPc6V0U7Qudkv1a1XLu0', { api_host: 'https://eu.i.posthog.com', person_profiles: 'always', disable_web_experiments: false  });
+
+// Define plan prices
+const planPrices = {
+    'FREE': 0,
+    'PREMIUM': 9.99,
+    'MAX-IMAL': 19.99
+};
 
 // Track page views
 posthog.capture('$pageview');
@@ -16,5 +24,23 @@ function identifyUser(userEmail) {
     posthog.identify(userEmail);
 }
 
+// Track plan selection
+function trackPlanSelection(planName) {
+    const normalizedPlanName = planName.toUpperCase();
+    posthog.capture('plan_selected', {
+        plan_name: normalizedPlanName,
+        plan_price: planPrices[normalizedPlanName] || 'Unknown'
+    });
+}
+
+// Track plan purchase
+function trackPlanPurchase(planName) {
+    const normalizedPlanName = planName.toUpperCase();
+    posthog.capture('plan_purchased', {
+        plan_name: normalizedPlanName,
+        plan_price: planPrices[normalizedPlanName] || 'Unknown'
+    });
+}
+
 // Export functions for use in other scripts
-export { trackButtonClick, identifyUser };
+export { trackButtonClick, identifyUser, trackPlanSelection, trackPlanPurchase };
